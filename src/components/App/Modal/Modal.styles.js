416f9a0d3a3171b9e6cff32297930ac1de142b6c
alekry78 <krysaleksander1@gmail.com>
2021-05-14 23:16:51 +0200

    deploy

--- src/components/App/Modal/Modal.styles.js
+++ src/components/App/Modal/Modal.styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 export const ModalView = styled.div`
-    width:100vw;
-    height:100vh;
+    width:100%;
+    height:100%;
     display:flex;
     flex-direction:column;
     justify-content:center;
